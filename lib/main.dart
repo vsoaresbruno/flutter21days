6bf357ccf377e8e088e52d5c577cef329707d42b
Bruno Soares <vsoares.bruno@gmail.com>
2020-11-10 18:51:57 -0300

    feat: added Stack positioned

--- lib/main.dart
+++ lib/main.dart
@@ -31,37 +31,34 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(widget.title),
-      ),
-      body: Container(
-        child: ListView(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Image from assets',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            Image.asset('images/detective-pikachu.jpg'),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Text(
-                'Image from network',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            Image.network(
-              'https://images.pexels.com/photos/4245856/pexels-photo-4245856.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-              loadingBuilder: (context, child, progress) {
-                return progress == null ? child : LinearProgressIndicator();
-              },
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(widget.title),
         ),
-      ),
-    );
+        body: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              right: 0,
+              left: 0,
+              child: Container(color: Colors.black, height: 200),
+            ),
+            Positioned(
+              top: 100,
+              right: 50,
+              left: 50,
+              child: Image.asset('images/detective-pikachu.jpg'),
+            ),
+            Positioned(
+              top: 350,
+              right: 10,
+              left: 10,
+              child: Text(
+                'You can do wonders with Stack',
+                style: TextStyle(fontSize: 30),
+              ),
+            )
+          ],
+        ));
   }
 }
